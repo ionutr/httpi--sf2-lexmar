c5410e6679d5e5e0a8fd504799a454bf3c309fbc
Ionut Rus <hi@ionutrus.com>
2013-12-25 01:09:10 +0100

    fixed

--- src/Httpi/Maritime/Bundle/ImportBundle/Library/Import/LexiconImport.php
+++ src/Httpi/Maritime/Bundle/ImportBundle/Library/Import/LexiconImport.php
@@ -41,6 +41,7 @@ class LexiconImport extends FileImport {
         // call parent core execution first
         $this->keepTransaction(true);
         parent::execute();
+        $this->beginTransaction();
 
         // iterate registered file ids
         foreach ($this->fileIds as $fileId) {
